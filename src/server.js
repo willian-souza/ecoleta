5f33abaac33906469b6451ef5e73f01ed70bee22
Willian dos Santos Souza <willian.santos.souza@live.com>
2020-06-04 13:16:47 -0300

    Criando e configurando o backend

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const server = express()
+
+//configurar pasta pública
+server.use(express.static("public"))
+
+
+//utilizando templates engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express:server,
+    noCache: true
+})
+
+
+
+//configurar caminhos da minha aplicação
+//página inicial
+server.get("/", (req,res) => {
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req,res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search-results", (req,res) => {
+    return res.render("search-results.html")
+})
+
+//ligar o servidor
+server.listen(3000)
